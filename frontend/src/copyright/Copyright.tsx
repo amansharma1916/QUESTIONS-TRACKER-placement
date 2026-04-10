e16dd8126abcad4a474410317613b3e4f8912ed4
Aman Sharma <166890222+amansharma1916@users.noreply.github.com>
2026-04-10 14:35:35 +0530

    Update Copyright.tsx

--- frontend/src/copyright/Copyright.tsx
+++ frontend/src/copyright/Copyright.tsx
@@ -1,8 +1,27 @@
-import './Copyright.css'
-const Copyright = () => {
-  return (
-    <div className='copyright-text'>Developer - Aman Sharma</div>
-  )
-}
+import { useEffect } from 'react';
+import './Copyright.css';
 
-export default Copyright    
+const Copyright = () => {
+
+  useEffect(() => {
+    const checkHealth = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/health`);
+        const data = await response.json();
+        console.log('Health API response:', data);
+      } catch (error) {
+        console.error('Health API error:', error);
+      }
+    };
+
+    checkHealth();
+  }, []);
+
+  return (
+    <div className='copyright-text'>
+      Developer - Aman Sharma
+    </div>
+  );
+};
+
+export default Copyright;
